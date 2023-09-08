cb5718e29a1ab933f686b94a162f980a2265c3ea
kavivanank <91777538+kavivanank@users.noreply.github.com>
2023-09-08 14:49:54 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"  
+}
+
+resource "aws_instance" "foo" {
+  ami           = "ami-0f5ee92e2d63afc18" # ap-south-1
+  instance_type = "t2.micro"  
+   tags = {
+      Name = "Test-Server"-2
+  }
+}
